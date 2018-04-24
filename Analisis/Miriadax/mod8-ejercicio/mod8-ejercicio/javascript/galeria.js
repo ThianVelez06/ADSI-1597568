49b61fcf62300a3037e90d5b87e57a9e63777d86
JuanSeVelez <holamax1234@gmail.com>
2018-04-24 10:45:59 -0500

    Evidences
    Here is the evidence from the analysis phase

---
+++ Analisis/Miriadax/mod8-ejercicio/mod8-ejercicio/javascript/galeria.js
@@ -0,0 +1,26 @@
+var galeria = [
+   { persona: "Buddha - बुद्धा",
+    frase: "En la confrontación entre el arrollo y la roca, el arrollo siempre ganará; no por la fuerza, sino por la persistencia.",
+    foto: "images/buda.jpg"
+   },
+   { persona: "Khalil Gibran - جبران خليل جبران بن ميخائل بن سعد",
+    frase: "El silencio del envidioso está lleno de ruidos.",
+    foto: "images/gibran.jpg"
+   },
+   { persona: "Confucio - 孔子",
+    frase: "Todo tiene belleza, pero no todo el mundo la puede ver.",
+    foto: "images/confucio.jpg"
+   },
+   { persona: "Lev Nikoláievich Tolstói - Лев Николаевич Толстой",
+    frase: "Mi felicidad consiste en que sé apreciar lo que tengo y no deseo, con exceso, lo que no tengo.",
+    foto: "images/tolstoi.jpg"
+   },
+   { persona: "Platón - Πλάτων",
+    frase: "El más importante y principal negocio público es la buena educación de la juventud.",
+    foto: "images/platon.jpg"
+   },
+   { persona: "Henrik Ibsen - hɛnɾɪk ˈjoːhɑn ˈɪpsən",
+    frase: "Si dudas de ti mismo, estás vencido de antemano.",
+    foto: "images/ibsen.jpg"
+   }
+];
